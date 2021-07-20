0f16a473cf66292ffc3554702d25dca5138810ee
ovi <ovidiu.mirzac@gmail.com>
2021-07-20 21:05:32 +0300

    main example without lambda

---
+++ src/com/sda/lectia9/lambda/mainexample/withoutlambda/Main.java
@@ -0,0 +1,14 @@
+/*
+package com.sda.lectia9.lambda.mainexample.withoutlambda;
+
+import java.util.function.Predicate;
+
+public class AdultPersonValidation implements Predicate<Person> {
+    public AdultPersonValidation() {
+    }
+
+    public boolean test(Person person) {
+        return person.getAge() >= 18;
+    }
+}
+*/
